e7b61534f73059f136d219de1bb1d64cb4e43a77
greg <gregory.cusack@colorado.edu>
2022-05-29 19:07:11 -0500

    signing and validating working

--- tutorial.py
+++ tutorial.py
@@ -12,13 +12,37 @@ load_dotenv()
 w3 = Web3(Web3.HTTPProvider(os.getenv("GANACHE_RPC_URL")))
 chain_id = 1337
 
-msg = "I♥SF"
 my_address = os.getenv("ACCOUNT_ADDRESS")
 private_key = os.getenv("PRIVATE_KEY")
-private_key = w3.toHex(hexstr=private_key)
-# private_key = private_key.encode('ISO-8859-1')
 
+abi = json.loads(
+    compiled_sol["contracts"]["IANA.sol"]["IANA"]["metadata"]
+    )["output"]["abi"]
 
+#  call deploy.py Will get contract_address
+contract_address = os.getenv("CONTRACT_ADDRESS")
+iana = w3.eth.contract(address=contract_address, abi=abi)
+
+base_message = Web3.soliditySha3(['uint32', 'address'], [13, my_address])
+print("msg from solidity sha: " + str(base_message))
+message = encode_defunct(primitive=base_message)
+signed_message = w3.eth.account.sign_message(message, private_key=private_key)
+print("(base_message, messagehash): " + str(base_message) + ", " + str(signed_message.messageHash))
+
+base_message_2, message_hash_2 = iana.functions.IANA_getSignatureMessageTest(13, my_address).call()
+
+print("(base_message_2, messagehash_2): " + str(base_message_2) + ", " + str(message_hash_2))
+
+print(base_message == base_message_2)
+print(signed_message.messageHash == message_hash_2 )
+
+######################################################
+####################################################
+
+"""
+TODO: UNCOMMENT ALL BELOW (HIGHLIGHT AND COMMAND /)
+"""
+msg = "I♥SF"
 message = encode_defunct(text=msg)
 signed_message = w3.eth.account.sign_message(message, private_key=private_key)
 print(signed_message)
@@ -49,23 +73,23 @@ print("---------------------------")
 # THIS is case, where we have received a message and a signature encoded to hex.
 # This prepares data for solidity
 # So i think this is like IANA receiving the data from a user to call a owners only function
-# hex_message = '0x49e299a55346' #idk what this is
-# hex_signature = signed_message.signature.hex()
+hex_message = '0x49e299a55346' #idk what this is
+hex_signature = signed_message.signature.hex()
 
-# message = encode_defunct(hexstr=hex_message)
+message = encode_defunct(hexstr=hex_message)
 
-# message_hash = _hash_eip191_message(message)
-# hex_message_hash = Web3.toHex(message_hash)
+message_hash = _hash_eip191_message(message)
+hex_message_hash = Web3.toHex(message_hash)
 
-# sig = Web3.toBytes(hexstr=hex_signature)
-# v, hex_r, hex_s = Web3.toInt(sig[-1]), Web3.toHex(sig[:32]), Web3.toHex(sig[32:64])
+sig = Web3.toBytes(hexstr=hex_signature)
+v, hex_r, hex_s = Web3.toInt(sig[-1]), Web3.toHex(sig[:32]), Web3.toHex(sig[32:64])
 
-# ec_recover_args = (hex_message_hash, v, hex_r, hex_s)
-# print(ec_recover_args)
+ec_recover_args = (hex_message_hash, v, hex_r, hex_s)
+print(ec_recover_args)
 
 
-print(ec_recover_args[0])
-print(type(ec_recover_args[0]))
+# print(ec_recover_args[0])
+# print(type(ec_recover_args[0]))
 
 
 
@@ -81,7 +105,7 @@ contract_address = os.getenv("RECOVER_CONTRACT_ADDRESS")
 #  Instantiate the contract object 
 recover = w3.eth.contract(address=contract_address, abi=abi)
 
-print(type(ec_recover_args[0]), type(ec_recover_args[1]), type(ec_recover_args[2]), type(ec_recover_args[3]) )
+# print(type(ec_recover_args[0]), type(ec_recover_args[1]), type(ec_recover_args[2]), type(ec_recover_args[3]) )
 addr = recover.functions.ecr(ec_recover_args[0], ec_recover_args[1], ec_recover_args[2], ec_recover_args[3]).call()
 
 print(addr)
