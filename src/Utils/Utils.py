55e843c70cac65efabebe8191524990fc5724824
greg <gregory.cusack@colorado.edu>
2022-08-02 21:19:41 -0600

    add compile/deploy functionality flexibility. start path validation contract

--- src/Utils/Utils.py
+++ src/Utils/Utils.py
@@ -3,6 +3,7 @@ from web3 import Web3
 from eth_account.messages import encode_defunct
 from dotenv import load_dotenv
 from eth_account.messages import encode_defunct, _hash_eip191_message
+from Config import ROOT_DIR
 import os
 import json
 from enum import Enum
@@ -97,9 +98,12 @@ class Utils(object):
         :return: contract abi
         """
 
-        return json.loads(
-            compiled_sol["contracts"][contract_name + ".sol"][contract_name]
-            ["metadata"])["output"]["abi"]
+        compiled_json_path = os.path.join(ROOT_DIR, 'compiled_json/compiled_' + contract_name + '_code.json')
+        with open(compiled_json_path, "r") as f:
+            compiled_sol = json.load(f)
+            return json.loads(
+                compiled_sol["contracts"][contract_name + ".sol"][contract_name]
+                ["metadata"])["output"]["abi"]
 
     #  Compiled bytecode of smart contract. Needed to deploy contract
     @staticmethod
@@ -107,7 +111,10 @@ class Utils(object):
         """
         Returns contract's bytecode. Need to deploy contract and get address
         """
-        return compiled_sol["contracts"][contract_name + ".sol"][contract_name]["evm"]["bytecode"]["object"]
+        compiled_json_path = os.path.join(ROOT_DIR, 'compiled_json/compiled_' + contract_name + '_code.json')
+        with open(compiled_json_path, "r") as f:
+            compiled_sol = json.load(f)
+            return compiled_sol["contracts"][contract_name + ".sol"][contract_name]["evm"]["bytecode"]["object"]
 
 
     @staticmethod
--- src/deploy.py
+++ src/deploy.py
@@ -4,17 +4,18 @@ from ipaddress import IPv4Address
 import sys
 
 def main():
-    if len(sys.argv) < 2:
-        print("please enter a tx_sender (e.g. ACCOUNT0) to deploy contract from")
+    if len(sys.argv) < 3:
+        print("please enter a tx_sender (e.g. ACCOUNT0) to deploy contract from and a contract name (e.g. IANA, PATH_VALIDATION)")
         sys.exit(-1)
 
     tx_sender_name = str(sys.argv[1])
+    contract_to_deploy = str(sys.argv[2])
 
     # create accounts
     tx_sender = Account(AccountType.TransactionSender, tx_sender_name)
     tx_sender.load_account_keys()
 
-    tx_sender.generate_deploy_contract_object("IANA")
+    tx_sender.generate_deploy_contract_object(contract_to_deploy)
 
     # Generate deploy contract transaction object
     tx = tx_sender.tx.deploy_smart_contract(tx_sender.get_nonce())
@@ -24,7 +25,7 @@ def main():
     if TxErrorType(err) != TxErrorType.OK:
         print("ERROR: " + str(TxErrorType(err)) + ". Contract NOT deployed. Transaction failed")
         
-    print("SUCCESS: Contract deployed")
+    print("SUCCESS: " + contract_to_deploy + " Contract deployed")
     print("Contract address: " + tx_receipt.contractAddress)
 
 if __name__ == "__main__":
