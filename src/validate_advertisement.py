7f178ade0cdda162a54bcd277d635eedabba43f5
greg <gregory.cusack@colorado.edu>
2022-08-02 22:51:48 -0600

    working. but there is a little hack. see validate_advertisement.py

--- src/validate_advertisement.py
+++ src/validate_advertisement.py
@@ -22,50 +22,64 @@ A3: 10.0.20.0/24 : 3, 2, 1
 
 """
 
+"""
+Example
+1 -> 2 -> 3 -> 4 -> ...
+python add_advertisement.py ACCOUNT0 10.0.20.0 24 2
+python add_advertisement.py ACCOUNT1 10.0.20.0 24 3
+python add_advertisement.py ACCOUNT2 10.0.20.0 24 4
+
+Now validate
+
+python validate_advertisement.py ACCOUNT0 10.0.20.0 24 <myASN> <Received advertisement's AS_PATH>
+ex: 
+python validate_advertisement.py ACCOUNT0 10.0.20.0 24 2 1
+
+## check down path that path 2->3 exists and 1->2 exists
+python validate_advertisement.py ACCOUNT1 10.0.20.0 24 3 2 1
+
+## check down path that paths 3->4, 2->3, and 1->2 exist
+python validate_advertisement.py ACCOUNT1 10.0.20.0 24 4 3 2 1
+
+Caviate here: we assume ASN1 = contract0, ASN2 = contract1, etc
+"""
+
 def main():
-    if len(sys.argv) < 5:
-        print("please enter an tx_sender, ip, subnet, your own ASN, and the advertisement's AS_PATH (e.g. 3 2 1) to validate path")
+    len_args = len(sys.argv)
+    if len_args < 6:
+        print("please enter an tx_sender, ip, subnet, your own ASN, and the incoming advertisement's AS_PATH (e.g. 3 2 1) to validate path")
         sys.exit(-1)
 
     tx_sender_name = str(sys.argv[1])
     inIP = IPv4Address(sys.argv[2])
     inSubnet = int(sys.argv[3])
-    inPrevHop = int(sys.argv[4])
     myASN = int(sys.argv[4])
+
+    AS_PATH = []
+    contract_addresses = []
+    for i in range(5, len_args):
+        asn = sys.argv[i]
+        AS_PATH.append(int(asn))
+        contract_addresses.append("ACCOUNT" + str(int(asn)-1) + "_PATH_VALIDATION_CONTRACT")
+
+    asn_contract_zip = zip(AS_PATH, contract_addresses)
 
     # create accounts
     tx_sender = Account(AccountType.TransactionSender, tx_sender_name)
     tx_sender.load_account_keys()
 
-    # we actually need to pass in the contract address here. or we can do something with a config file
-    tx_sender.generate_transaction_object("PATH_VALIDATION", "MY_PATH_VALIDATION_CONTRACT_ADDRESS")
+    print(AS_PATH)
+    print(contract_addresses)
 
-    # data to hash and sign
-    data_types = ['uint32', 'uint8', 'uint32']
-    data = [int(inIP), inSubnet, inPrevHop]
-
-    tx = tx_sender.tx.sc_validateAdvertisement(int(inIP), inSubnet, inPrevHop)
-    print(tx)
-    
-    # # sign and hash data
-    # signed_message, err = msg_sender.hash_and_sign_message(data_types, data)
-    # if err:
-    #     print("ERROR: failed to hash and sign message")
-    #     sys.exit(-1)
-
-    # # generate signed message validation data
-    # tx_sender.generate_signature_validation_data_from_signed_message(signed_message)
-
-    # # generate contract transaction
-    # tx = tx_sender.tx.sc_addASN(tx_sender.get_nonce(), inASN, inAddress)
-
-    # sign and execute transaction
-    # tx_hash, tx_receipt, err = tx_sender.tx.sign_and_execute_transaction(tx)
-    # if TxErrorType(err) != TxErrorType.OK:
-    #     print("ERROR: " + str(TxErrorType(err)) + ". Transaction NOT executed")
-        
-    # print("SUCCESS: ASN<=>Address added")
-
+    # need to load a map here that maps ASN to the ASN's path_validation contract address
+    # hack for now. ASN1 = contract0, ASN2 = contract1, etc
+    nextHop = myASN
+    for ASN, contract_address in asn_contract_zip:
+        # we actually need to pass in the contract address here. or we can do something with a config file
+        tx_sender.generate_transaction_object("PATH_VALIDATION", contract_address)
+        tx = tx_sender.tx.sc_validateAdvertisement(int(inIP), inSubnet, nextHop)
+        nextHop = ASN
+        print(tx)
 
 if __name__ == "__main__":
     main()
